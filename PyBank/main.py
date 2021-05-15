4b5c3a49f0bed255f6faf645a84b7544a70e58a4
Portia <as_posh@hotmail.com>
2021-05-15 01:53:07 -0400

    code update

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,6 +10,9 @@ l# Create a Python script that analyzes the PyBank records to calculate each of 
 import os
 import csv
 
+# Filepath to collect output data
+budget_data_csv_path = os.path.join("Resources", "budget_data.csv")
+
 # Define PyBank's variables
 total_months = 0
 total_profit_loss = 0
@@ -18,14 +21,15 @@ average_profit_loss_change = []
 previous_month_profit_loss = 0
 current_month_profit_loss = 0
 
-# Path to collect data from the Resources folder
-budget_data_csv_path = os.path.join("Resources", "budget_data.csv")
+# Open and read csv file, use DictReader to allow for columns to be referenced by name instead of index numbers
+with open(budget_data_csv_path) as budget:
+    reader - csv.DictReader(budget_data)
 
-    for row in csv_reader
+    for row in csvreader:
          
         #Convert string values to integers, iterate through rows and calculate totals
-        total_months = total months + 1
-        total_profit_loss = total profit_loss + int(row["Profit/Loss"])
+        total_months = total_months + 1
+        total_profit_loss = total_profit_loss + int(row["Profit/Loss"])
 
         #Calculate profit_loss_changes
         profit_loss_change = int(row["Profit/Loss"]) - previuos_profit_loss
@@ -38,11 +42,16 @@ budget_data_csv_path = os.path.join("Resources", "budget_data.csv")
                                 
         #Calulate the greatest increase
         if(profit_loss_change > greatest_increase [1]):
-            greatest_increase [0[ = row["Date"]
-            greatest_increase [1] = profit_loss_change
+            greatest_increase [0[ = str(row[0])
+            greatest_increase [1] = change_value
 
         #Calculate the greatest decrease
         if(profit_loss_change) < greatest_decrease [1]:
-            greatest_decrease [0] = row["Date"]
-            greatest_decrease[1] = profit_loss_change
-    
+            greatest_decrease [0] = str (row[0])
+            greatest_decrease[1] =  change_value
+
+print(f" Total Months: {total_months}")
+print(f" Total: {sum(pnl_statements)}")
+print(f" Average Change: {sum(pnl_statements) / total_months}")
+print(f" Greatest increase in profits: {greatest_increase_month} ({greatest_increase})")
+print(f" Greatest decrease in profits: {greatest_increase_month} ({greatest_decrease})")
